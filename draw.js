01c0b630b1d77505a895f6b749da77fcd02b8c72
JoshCircenis <98178221+JoshCircenis@users.noreply.github.com>
2023-06-22 20:52:30 -0600

    06/22/2023 update
    All current features are working and the style.css has been completed.

---
+++ draw.js
@@ -0,0 +1,104 @@
+const grid = document.getElementById("grid");
+const clear = document.getElementById('clear');
+clear.addEventListener('click', resetGrid);
+const cells = [];
+
+var inputLayer = [];
+
+for (let i = 0; i < 3600; i++) {
+   inputLayer[i] = 0; 
+}
+
+var x = "test";
+
+// Create grid cells
+for (let i = 0; i < 3600; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cells.push(cell);
+  grid.appendChild(cell);
+}
+
+let isDrawing = false;
+
+// Mouse event handlers
+grid.addEventListener("mousedown", (event) => {
+  isDrawing = true;
+  const cell = event.target;
+  cell.style.backgroundColor = "#000";
+  logState(cell);
+  const neighboringCells = getNeighboringCells(cell);
+  neighboringCells.forEach((neighboringCell) => {
+    neighboringCell.style.backgroundColor = "#000";
+    logState(neighboringCell);
+  });
+});
+
+grid.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+grid.addEventListener("mouseleave", () => {
+  isDrawing = false;
+});
+
+grid.addEventListener("mousemove", (event) => {
+  if (isDrawing) {
+    const cell = event.target;
+    cell.style.backgroundColor = "#000";
+    logState(cell);
+    const neighboringCells = getNeighboringCells(cell);
+    neighboringCells.forEach((neighboringCell) => {
+      neighboringCell.style.backgroundColor = "#000";
+      logState(neighboringCell);
+    });
+  }
+});
+
+function logState(cell){
+  const index = cells.indexOf(cell);
+  inputLayer[index] = 1;
+  //const row = Math.floor(index / 60);
+  //const col = index % 60;
+
+  //inputLayer[row][col] = 1;
+}
+
+function getNeighboringCells(cell) {
+  const neighboringCells = [];
+  const index = cells.indexOf(cell);
+  const row = Math.floor(index / 60);
+  const col = index % 60;
+
+  if (row > 0) {
+    neighboringCells.push(cells[index - 60]); // Top
+  }
+  if (row < 59) {
+    neighboringCells.push(cells[index + 60]); // Bottom
+  }
+  if (col > 0) {
+    neighboringCells.push(cells[index - 1]); // Left
+  }
+  if (col < 59) {
+    neighboringCells.push(cells[index + 1]); // Right
+  }
+  if((row > 0)&&(col >0)){
+    neighboringCells.push(cells[index - 61]); // Top left
+    neighboringCells.push(cells[index + 59]); // Bottom left
+  }
+  if((row < 59)&&(col < 59)){
+    neighboringCells.push(cells[index - 59]); // Top right
+    neighboringCells.push(cells[index + 61]); // Bottom Right
+  }
+
+  return neighboringCells;
+}
+
+function resetGrid() {
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "#fff";
+  });
+  for (let i = 0; i < 3600; i++) {
+    inputLayer[i] = 0;
+  }
+}
---
+++ ml.js
@@ -0,0 +1,92 @@
+const guess = document.getElementById('guess');
+const label = document.getElementById('charSelect');
+const output = document.getElementById('output');
+const learn = document.getElementById('train');
+learn.addEventListener('click', learnFUNC);
+guess.addEventListener('click', guessFUNC);
+
+
+//create model
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 128, activation: 'relu', inputShape: [3600] }));
+model.add(tf.layers.dense({ units: 26, activation: 'softmax' }));
+
+//compile model
+model.compile({
+  optimizer: 'adam',
+  loss: 'categoricalCrossentropy',
+  metrics: ['accuracy'],
+});
+
+//store training data
+var trainingData = [];
+var trainingLabels = [];
+
+//function to train model
+function learnFUNC(){
+  //output to user
+  output.innerHTML = "Training...";
+  
+  //1x3600 array from grid containing 1 for a black square and 0 for white square
+  trainingData.push(inputLayer);
+  //value from dropdown for which character is being trained
+  trainingLabels.push(parseInt(label.value));
+
+  //convert input data array into tensor 
+  const inputTensor = tf.tensor2d(trainingData, [trainingData.length,3600]);
+
+  //convert label to one-hot encoded tensors
+  const labelsTensor = tf.oneHot(trainingLabels, 26);
+
+  //train the model 
+  model.fit(inputTensor, labelsTensor, {
+    batchSize: 32,
+    epochs: 10,
+    shuffle: true,
+    callbacks: {
+      onEpochEnd: (epoch, logs) => {
+        console.log(`Epoch ${epoch + 1}: loss = ${logs.loss.toFixed(4)}, accuracy = ${logs.acc.toFixed(4)}`);
+      }
+    } 
+  })
+  .then((history) => {
+    console.log(history);
+  });
+  //output to user
+  output.innerHTML = "Training complete!";
+}
+
+function guessFUNC(){
+  //get input data array and convert to tensor
+  const inputData = inputLayer;
+  const inputTensor = tf.tensor2d(inputData, [1,3600]);
+
+  //make prediction and output to console
+  const prediction = model.predict(inputTensor);
+  prediction.print();
+
+  //convert prediction for output to user (displays top three predictions and confidence)
+  const results = prediction.dataSync();
+  const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  
+  const indecies = greatestConfidenceAnswers(results);
+  
+  output.innerHTML = "<pre>" + alphabet[indecies[0]]+" = "+(results[indecies[0]]*100).toFixed(2)+"%\n"+alphabet[indecies[1]]+" = "+(results[indecies[1]]*100).toFixed(2)+"%\n"+alphabet[indecies[2]]+" = "+(results[indecies[2]]*100).toFixed(2)+"%</pre>";
+}
+
+function greatestConfidenceAnswers(array) {
+  //create a copy of the array to avoid modifying the original array
+  const copyArray = [...array];
+
+  //sort the array in descending order
+  copyArray.sort((a, b) => b - a);
+
+  //get the indices of the three greatest elements
+  const indices = [];
+  for (let i = 0; i < 3; i++) {
+    const index = array.indexOf(copyArray[i]);
+    indices.push(index);
+  }
+
+  return indices;
+}
